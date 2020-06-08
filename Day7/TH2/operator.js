6564db54b6bdeac2e33f4c62852645b7c0cf333f
Mon0106 <dongnam.japan@gmail.com>
2020-06-08 10:17:18 +0700

    Sử dụng toán tử

---
+++ Day7/TH2/operator.js
@@ -0,0 +1,11 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width")
+inputHeight = prompt("Enter the width")
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Enter width: " + inputWidth+ '<br/>');
+document.write("Enter height: " + inputHeight+ '<br/>');
+document.write("The area is: " + area+ '<br/>');
+
