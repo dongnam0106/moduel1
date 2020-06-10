ec75752582dcbee1fa617e40a4702e7f3e9f25cc
Mon0106 <dongnam.japan@gmail.com>
2020-06-10 09:11:02 +0700

    game puzzle

---
+++ Day9/puzzle.js
@@ -0,0 +1,46 @@
+let n = 0;
+function  changeImg(e) {
+    n++;
+    let image1="../images/first"+n+".jpg";
+    console.log(e);
+    e.src = image1;
+    if(n>=3){
+        n = 0;
+    }
+}
+function  changeImg1(e) {
+    n++;
+    let image1="../images/two"+n+".jpg";
+    console.log(e);
+    e.src = image1;
+    if(n>=3){
+        n = 0;
+    }
+}
+function  changeImg2(e) {
+    n++;
+    let image1="../images/three"+n+".jpg";
+    console.log(e);
+    e.src = image1;
+    if(n>=3){
+        n = 0;
+    }
+}
+function  changeImg3(e) {
+    n++;
+    let image1="../images/four"+n+".jpg";
+    console.log(e);
+    e.src = image1;
+    if(n>=3){
+        n = 0;
+    }
+}
+function  changeImg4(e) {
+    n++;
+    let image1="../images/five"+n+".jpg";
+    console.log(e);
+    e.src = image1;
+    if(n>=3){
+        n = 0;
+    }
+}
