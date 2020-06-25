bd447e9b6daa035520154157b06e6a2ffcd14ca5
Mon0106 <dongnam.japan@gmail.com>
2020-06-25 14:18:13 +0700

    tạo hang chuột

---
+++ project/init.js
@@ -0,0 +1,8 @@
+
+function init() {
+    for (let i=0; i<4; i++) {
+        for (let j = 0; j < 4; j++) {
+            document.getElementById("i12").src= "imgs/hang_chuot.png";
+        }
+    }
+}
