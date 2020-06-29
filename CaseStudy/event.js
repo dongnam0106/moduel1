2753e36ab5a3765ab085f01f83f551271af644f9
Mon0106 <dongnam.japan@gmail.com>
2020-06-29 23:23:40 +0700

    Bỏ game :((

---
+++ CaseStudy/event.js
@@ -0,0 +1,6 @@
+function getLocal() {
+    localStorage.studynumber = document.getElementById("StudyNumber");
+    localStorage.name = document.getElementById("StudyName");
+    localStorage.pass = document.getElementById("StudyPass");
+    localStorage.email = document.getElementById("StudyEmail");
+}
